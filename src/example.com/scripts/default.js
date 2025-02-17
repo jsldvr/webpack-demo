048fb0411c11f265ba86695cc4a7e984e2da4a43
Jorge Saldivar <14043861+jsldvr@users.noreply.github.com>
2025-02-16 19:08:11 -0600

    refactor: remove createElement utility; update dependencies and integrate MiniCssExtractPlugin for CSS handling

--- src/example.com/scripts/default.js
+++ src/example.com/scripts/default.js
@@ -5,6 +5,9 @@
  * @since 1.0.0
  */
 
+// impot bootstrap v5.3.3 css 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export function test(params) {
     console.log('test');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 const path = require('path');
 const html_webpack_plugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack');
 
 /**
  * Semantic versioning
  * @link https://semver.org/
  */
-const version = '0.0.1-alpha' // See CHANGELOG.md for version history
+const version = '0.1.1-alpha' // See CHANGELOG.md for version history
 
 module.exports = (env) => {
+    // const is_production = argv.mode === 'production';
     const src_domain_config = require(path.join(__dirname, `src/${env.project}/config.js`));
     const project_name = env && env.project ? env.project : 'default';
     const dist_path = env && env.stage ? 'stage' : 'public';
 
     return {
         entry: [
-            // 'bootstrap/dist/css/bootstrap.min.css',
             `./src/${project_name}/index.js`
         ],
         output: {
-            filename: 'bundle.js',
+            filename: 'bundle.js?name=[name]&ch=[contenthash]',
             path: path.resolve(__dirname, 'dist', dist_path, project_name),
-            // publicPath: '/'
         },
         plugins: [
             new html_webpack_plugin({
@@ -29,15 +29,21 @@ module.exports = (env) => {
                 filename: 'index.html',
                 inject: 'body',
             }),
+            new MiniCssExtractPlugin({
+                filename: "default.css?name=[name]&ch=[contenthash]",
+                chunkFilename: "chuck.css?id=[id]&ch=[contenthash]"
+            }),
             new webpack.DefinePlugin({
                 'process.env': JSON.stringify(src_domain_config),
-            }),
+            })
         ],
         module: {
             rules: [
                 {
                     test: /\.css$/i,
-                    use: ["style-loader", "css-loader"]
+                    use: [
+                        MiniCssExtractPlugin.loader, "css-loader"
+                    ]
                 },
                 {
                     test: /\.(png|jpg|gif)$/,
